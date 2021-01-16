df18847e1af1f97ff974eca3aef8f39fdbff79e2
mamezou <mamezou.king@gmail.com>
2021-01-16 20:22:43 +0900

    Axios追加。おまけとしてアバウトページも入れておいた。

---
+++ pages/blog/_slug/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+// ここに5-2でメモしたキーを入れると、右側のバリューが出力されます。画像などの表示は
+    <h1>{{ title }}</h1>
+    <p>{{ date }}</p>
+    <div v-html="contents"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://あなたのサービスID.microcms.io/api/v1/blog/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'あなたのAPIキー' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <h1>ブログ一覧</h1>
+    <ul>
+// 以下「contents」は5-2でレスポンスで登場した、jsonデータはじめの「contents」のことです。
+      <li v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/blog/${content.id}`">
+          {{ content.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </main>
+</template>
+<script>
+import axios from "axios";
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // 5-2でメモしたURLを記載しましょう。
+      "https://あなたのサービスID.microcms.io/api/v1/blog",
+      {
+        // 5-2でメモしたAPIキーを記載しましょう。
+        headers: { "X-API-KEY": "あなたのAPIキー" }
+      }
+    );
+    return data;
+  }
+};
+</script>
+<style></style>
